4cdbb5f50da59e0232ccd1c974ee66eee7b7fcfe
Lian <lcb_faust@naver.com>
2023-11-23 23:52:45 +0900

    mypage 뷰단 작업

---
+++ app/src/main/java/com/example/my_universe/recycler/MyPageAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.my_universe.recycler
+
+import android.content.Context
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.my_universe.MyPageActivity
+import com.example.my_universe.R
+import com.example.my_universe.databinding.ActivityMyPageBinding
+import com.example.my_universe.model.MyPageItem
+
+class MyPageViewHolder (val binding: ActivityMyPageBinding) : RecyclerView.ViewHolder(binding.root)
+class MyPageAdapter(val datas: ArrayList<Uri>, val context: Context)
+    : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        MyPageViewHolder(
+            ActivityMyPageBinding.inflate(
+                LayoutInflater.from(parent.context),parent,false))
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
