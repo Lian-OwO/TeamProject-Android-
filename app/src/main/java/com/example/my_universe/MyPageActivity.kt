43d8db37c7ad7090b3c86f700c70e25d571b5075
Lian <lcb_faust@naver.com>
2023-11-23 23:59:01 +0900

    Revert "mypage 뷰단 작업"
    This reverts commit 4cdbb5f50da59e0232ccd1c974ee66eee7b7fcfe.

--- app/src/main/java/com/example/my_universe/MyPageActivity.kt
+++ app/src/main/java/com/example/my_universe/MyPageActivity.kt
@@ -2,13 +2,10 @@ package com.example.my_universe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.my_universe.databinding.ActivityMyPageBinding
 
 class MyPageActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMyPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMyPageBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_my_page)
     }
 }
--- app/src/main/java/com/example/my_universe/model/MyPageItem.kt
+++
@@ -1,5 +0,0 @@
-package com.example.my_universe.model
-
-data class MyPageItem(
-    val text:String
-)
