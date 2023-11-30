f2940d1d49acbddcc8ae97cc3e342c5123164063
강정현 <life.us0410@gmail.com>
2023-11-30 16:15:47 +0900

    회원가입/번호인증 완료

--- app/src/main/java/com/example/my_universe/model/User.kt
+++ app/src/main/java/com/example/my_universe/model/User.kt
@@ -1,9 +1,10 @@
 package com.example.my_universe.model
 
 import com.google.firebase.database.IgnoreExtraProperties
+import java.io.Serializable
 
 @IgnoreExtraProperties
-data class User(val name: String? = null, val email: String? = null,val password: String? = null,val passwordCheck: String? = null) {
+data class User(val name: String? = null, val email: String? = null,val password: String? = null,val phoneNum: String?) {
     // Null default values create a no-argument default constructor, which is needed
     // for deserialization from a DataSnapshot.
     //   val email = binding.joinEmail.text
